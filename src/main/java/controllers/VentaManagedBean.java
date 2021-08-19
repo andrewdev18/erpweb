50c6b36bbe9ff76a0cc09beb03724e92f0de56b1
andrewdev18 <85127763+andrewdev18@users.noreply.github.com>
2021-08-18 23:23:53 -0500

    New Update

--- src/main/java/controllers/VentaManagedBean.java
+++ src/main/java/controllers/VentaManagedBean.java
@@ -5,12 +5,31 @@
  */
 package controllers;
 
+import DataViews.ClienteVentaDao;
+import Models.ClienteVenta;
+import java.io.Serializable;
 import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+/**
+ *
+ * @author Andres
+ */
 @Named(value="VentaMB")
 @RequestScoped
-public class VentaManagedBean {
+public class VentaManagedBean implements Serializable{
+    private ClienteVenta cliente;
+    private ClienteVentaDao clienteDAO;
     
+    @PostConstruct
+    public void VentaManagedBean(){
+        cliente = new ClienteVenta();
+        clienteDAO = new ClienteVentaDao();
+    }
+       
+    public void BuscarCliente(String idnum){
+        this.cliente = clienteDAO.BuscarCliente(idnum);
+    }
 }
