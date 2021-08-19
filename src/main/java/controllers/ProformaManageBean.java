e749b105b89090fbe54e6fe0cad6cf27aebd3681
CristopherZambrano <79436828+CristopherZambrano@users.noreply.github.com>
2021-08-18 22:30:21 -0500

    Base de proformas

---
+++ src/main/java/controllers/ProformaManageBean.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named(value="VentaMB")
+@RequestScoped
+
+public class ProformaManageBean {
+    
+}
