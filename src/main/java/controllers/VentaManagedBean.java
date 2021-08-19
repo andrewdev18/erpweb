76cb75b3ab3ab5b661c3852b6bad874684bb0a28
andrewdev18 <85127763+andrewdev18@users.noreply.github.com>
2021-08-18 19:17:42 -0500

    Creacion de ClienteVentaDAO y inicio del Manage Bean para las ventas

---
+++ src/main/java/controllers/VentaManagedBean.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named(value="VentaMB")
+@RequestScoped
+public class VentaManagedBean {
+    
+}
